a881e64e19297116f230f48bfda45c34c04abc11
Ahmed Sharuvan <ahmed2.sharuvan@live.uwe.ac.uk>
2023-06-22 10:30:50 +0500

    newline support added

--- kernel.c
+++ kernel.c
@@ -85,11 +85,19 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
 }
 
 void terminal_putchar(char c) {
-  terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-  if (++terminal_column == VGA_WIDTH) {
+  // if input character is the newline character, increase
+  // row index and set column index to zero
+  if (c == '\n') {
+    terminal_row++;
     terminal_column = 0;
-    if (++terminal_row == VGA_HEIGHT) {
-      terminal_row = 0;
+  }
+  else {
+    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+    if (++terminal_column == VGA_WIDTH) {
+      terminal_column = 0;
+      if (++terminal_row == VGA_HEIGHT) {
+        terminal_row = 0;
+      }
     }
   }
 }
@@ -111,5 +119,7 @@ void kernel_main() {
    * yet, '\n' will produce some VGA specific character instead.
    * This is normal.
    */
+  terminal_writestring("Hello, kernel World!\n");
+  terminal_writestring("Hello, kernel World!\n");
   terminal_writestring("Hello, kernel World!\n");
 }
